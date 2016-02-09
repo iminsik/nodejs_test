e3581cc62732492e6c880f16e2d7e94289c4341b
Insik Cho <insik.cho@alaskaair.com>
2016-02-09 13:06:06 -0800

    cleaning

--- index.js
+++ index.js
@@ -41,32 +41,3 @@ app.post('/find', function(req, res) {
 app.set('port', process.env.PORT || 3000);
 
 app.listen(app.get('port'));
-
-
-/*
-parser.addListener('end', function(json) {
-    var nodesByName = document.getElementsByTagName('PricingSolution');    
-    
-    fs.writeFile(
-        __dirname + '/foo.json',
-        JSON.stringify(json, null, "  "),
-        function(err, data) {
-            if(!err) {
-                console.log('Converted and saved in foo.json!');
-            }        
-        }
-    );
-});
-
-fs.readFile(
-    __dirname + '/foo.xml',
-    {encoding: 'utf-8'},
-    function(err, data) {
-        var json;
-        if(!err) {
-            document = DOMParser.parseFromString(data);        
-            json = parser.parseString(data);
-        }
-    }
-);
-*/
