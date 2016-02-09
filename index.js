f30a1556bb4fb1cf713ed1412281a8aebb8ac9ff
Insik Cho <insik.cho@alaskaair.com>
2016-02-09 11:20:38 -0800

    Prepare to check XML elements by TagName

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
+/*global __dirname */
 var fs = require('fs'),
-    xml2js = require('xml2js');
-
+    xml2js = require('xml2js'),
+    DOMParser = new (require('xmldom')).DOMParser;
+var document;
 var parser = new xml2js.Parser();
-parser.addListener('end', function(result) {
-    fs.writeFile(__dirname + '/foo.json', JSON.stringify(result, null, "\t"), function(err, data) {
-        if(!err) {
-            console.log('Converted and saved in foo.json!');
-        }        
-    });
+
+parser.addListener('end', function(json) {
+
+    var nodesByName = document.getElementsByTagName('PricingSolution');    
+    
+    fs.writeFile(
+        __dirname + '/foo.json',
+        JSON.stringify(json, null, "\t"),
+        function(err, data) {
+            if(!err) {
+                console.log('Converted and saved in foo.json!');
+            }        
+        }
+    );
 });
 
-fs.readFile(__dirname + '/foo.xml', {encoding: 'utf-8'}, function(err, data) {
-    var json;
-    if(!err) {
-        json = parser.parseString(data);        
+fs.readFile(
+    __dirname + '/foo.xml',
+    {encoding: 'utf-8'},
+    function(err, data) {
+        var json;
+        if(!err) {
+            document = DOMParser.parseFromString(data);        
+            json = parser.parseString(data);
+        }
     }
-});
+);
