cf19bf1fd91f3d2233ddb886e4290c1af4229bdd
Insik Cho <insik.cho@alaskaair.com>
2016-02-16 16:29:26 -0800

    Compare BookingState In and Out

--- index.js
+++ index.js
@@ -25,12 +25,42 @@ app.get('/', function(req, res) {
 });
 
 app.post('/find', function(req, res) {
-    var doc = DOMParser.parseFromString(req.body.xmlstring);
-    var nodesByName = xpath.select(req.body.searchterm, doc);
+    var ret = '';
+    var docin = DOMParser.parseFromString(req.body.inxmlstring);
+    var docout = DOMParser.parseFromString(req.body.outxmlstring);
+    var nodesByNameIn = xpath.select(req.body.searchterm, docin);
+    var nodesByNameOut = xpath.select(req.body.searchterm, docout);
+    var columnNamesArray = [];
+    
     if (req.body.format === 'xml') {
-        writeXML(res, nodesByName.toString());
+        ret = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" + "<content>";
+        ret += nodesByNameIn.toString();
+        ret += nodesByNameOut.toString();
+        ret += "</content>";
+        
+        res.writeHead(200, {
+            'Content-Type': 'application/xml',
+            'Content-Length': ret.length
+        });
+        
+        res.end(ret);
+
     } else if (req.body.format === 'table') {
-        writeTable(res, nodesByName);
+        columnNamesArray = getColumnNames(nodesByNameIn, nodesByNameOut);
+        ret = '<html>';
+        ret += getHeader();
+        ret += '<br/>';
+        ret += writeTable(columnNamesArray[1], nodesByNameIn);
+        ret += '<br/>';
+        ret += writeTable(columnNamesArray[1], nodesByNameOut, columnNamesArray[0]);
+        ret += '</html>';
+        
+        res.writeHead(200, {
+            'Content-Type': 'text/html',
+            'Content-Length': ret.length
+        });
+        
+        res.end(ret);
     }    
 });
 
@@ -39,7 +69,7 @@ app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'));
 
 function writeXML(res, xmlstring) {
-    var sendDoc = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" + "<content>" + xmlstring + "</content>";
+    var sendDoc =  + xmlstring 
     res.writeHead(200, {
         'Content-Type': 'application/xml',
         'Content-Length': sendDoc.length
@@ -47,53 +77,53 @@ function writeXML(res, xmlstring) {
     res.end(sendDoc);
 }
 
-function writeTable(res, nodesByName) {
+function getHeader() {
+    return "<head><link rel='stylesheet' type='text/css' href='/public/css/style.css' /></head>";
+}
+function writeTable(columnNamesArray, nodesByName, columnNamesArrayCompared) {
     var i, j;
-    var ret = "<html>";
-    var columnNames = {};
+    var ret = '';
     var col;
     var find;
-    ret += "<head>"
-    + "<link rel='stylesheet' type='text/css' href='/public/css/style.css' />";
-    ret += "</head>";
 
-    for (i = 0; i < nodesByName.length; i++) {
-        for (j = 0; j < nodesByName[i].attributes.length; j++) {
-            columnNames[nodesByName[i].attributes[j].nodeName] = true;
-        }
-    }
-
-    ret += "<body>";
     ret += "<table style='border-collapse: collapse;'>";
     ret += "<tr>";
     ret += "<th class='cell'>" + "Search term" + "</th>";
-    for (col in columnNames) {
-        ret += "<th class='cell'>" + col + "</th>";
+
+    for (col in columnNamesArray) {
+        ret += "<th class='cell'>" + columnNamesArray[col] + "</th>";
     }
     ret += "</tr>";
 
     for (i = 0; i < nodesByName.length; i++) {
         ret += "<tr>";
         ret += "<th class='cell'>" + nodesByName[i].nodeName + "</th>";
-        // for (j = 0; j < nodesByName[i].attributes.length; j++) {
-        //     ret += "<td class='cell'>" + nodesByName[i].attributes[j].nodeValue + "</td>";
-        // }
-        for (col in columnNames) {
-            find = getAttr(nodesByName[i].attributes, col);
-            ret += "<td class='cell'>" + (find ? find : "") + "</td>";
+        for (col in columnNamesArray) {
+            find = getAttr(nodesByName[i].attributes, columnNamesArray[col]);
+            if(columnNamesArrayCompared) {
+                ret += "<td "
+                    + (hasColumn(columnNamesArrayCompared, columnNamesArray[col])
+                        ? "class='cell'>" : "class='cell different'>" )
+                    + (find ? find : "") + "</td>";
+            } else {
+                ret += "<td class='cell'>" + (find ? find : "") + "</td>";
+            }
         }
         ret += "</tr>";
     }
     ret += "</table>";
-    ret += "</body>";
-    ret += "</html>";
-    
-    res.writeHead(200, {
-        'Content-Type': 'text/html',
-        'Content-Length': ret.length
-    });
 
-    res.end(ret);
+    return ret;
+}
+
+function hasColumn(columnNamesArray, columnName) {
+    var i, len = columnNamesArray.length;
+    for(i = 0; i < len; i++) {
+        if(columnNamesArray[i] === columnName) {
+            return true;
+        }
+    }
+    return false;
 }
 
 function getAttr(attributes, search) {
@@ -104,4 +134,33 @@ function getAttr(attributes, search) {
       }
   }
   return null;
+}
+
+function getColumnNames(nodesByNameIn, nodesByNameOut) {
+    var columnNamesIn = {}, columnNamesOut = {};
+    var columnNamesArrayIn = [];
+    var columnNamesArrayOut = [];
+    var i, j, col;
+    
+    for (i = 0; i < nodesByNameIn.length; i++) {
+        for (j = 0; j < nodesByNameIn[i].attributes.length; j++) {
+            columnNamesIn[nodesByNameIn[i].attributes[j].nodeName] = true;
+        }
+    }
+
+    for (i = 0; i < nodesByNameOut.length; i++) {
+        for (j = 0; j < nodesByNameOut[i].attributes.length; j++) {
+            columnNamesOut[nodesByNameOut[i].attributes[j].nodeName] = true;
+        }
+    }
+
+    for (col in columnNamesIn) {
+        columnNamesArrayIn.push(col);
+    }
+
+    for (col in columnNamesOut) {
+        columnNamesArrayOut.push(col);
+    }
+    
+    return [columnNamesArrayIn.sort(), columnNamesArrayOut.sort()];
 }
